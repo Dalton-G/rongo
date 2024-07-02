6bd32d17d7e6941df1e7b79da9f8893f0eb96648
Dalton Gan <study.daltongan@gmail.com>
2024-07-02 23:34:49 +0800

    feat: added subfolder to reflect features

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
+import 'package:rongo/firebase_options.dart';
 import 'package:rongo/providers.dart';
 import 'package:rongo/routes.dart';
 import 'package:rongo/screen/onboarding/onboarding.dart';
@@ -10,7 +11,7 @@ import 'package:rongo/utils/theme/theme.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
