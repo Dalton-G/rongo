b42a1dd67d133475aba588261a5fd9ad2708924b
Dalton Gan <study.daltongan@gmail.com>
2024-07-21 11:21:16 +0800

    feat: add UI for recipe page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:rongo/screen/onboarding/onboarding.dart';
+import 'package:rongo/screen/recipe/recipe_homepage.dart';
+import 'package:rongo/screen/scan_and_add/scanner.dart';
 
 class Routes {
   Routes._();
 
   static const String onboarding = '/onboarding';
+  static const String scanner = '/scanner';
+  static const String recipeHomepage = '/recipe-homepage';
 
   static final dynamic routes = <String, WidgetBuilder>{
     onboarding: (BuildContext context) => const OnboardingPage(),
+    scanner: (BuildContext context) => const Scanner(),
+    recipeHomepage: (BuildContext context) => const RecipeHomePage(),
   };
 }
--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -11,34 +11,115 @@ class AppTheme {
   AppTheme._();
 
   // Colors 颜色
-  static const mercuryWhite = Color(0xFFF4F5F8);
-  static const nordicGrey = Color(0xFF222326);
-  static const boldRed = Color(0xFFCC0000);
+  // static const mercuryWhite = Color(0xFFF4F5F8);
+  // static const nordicGrey = Color(0xFF222326);
+  // static const boldRed = Color(0xFFCC0000);
+  //
+  //
+  // // Text Styles 文本样式
+  // static const TextStyle appBarTextStyle = TextStyle(
+  //   fontSize: 20.0,
+  //   color: mercuryWhite,
+  //   fontWeight: FontWeight.bold,
+  // );
+  //
+  // static const TextStyle titleLarge = TextStyle(
+  //   fontSize: 24.0,
+  //   color: mercuryWhite,
+  //   fontWeight: FontWeight.bold,
+  // );
+  //
+  // static const TextStyle bodyLarge = TextStyle(
+  //   fontSize: 16.0,
+  //   color: nordicGrey,
+  //   fontWeight: FontWeight.bold,
+  // );
+  //
+  // // AppBar
+  // static const AppBarTheme _lightAppBarTheme = AppBarTheme(
+  //   backgroundColor: nordicGrey,
+  //   foregroundColor: mercuryWhite,
+  //   titleTextStyle: appBarTextStyle,
+  //   elevation: 0,
+  //   iconTheme: IconThemeData(color: mercuryWhite),
+  // );
+  //
+  // // Color Scheme
+  // static const ColorScheme _lightColorScheme = ColorScheme.light(
+  //   primary: nordicGrey,
+  //   secondary: nordicGrey,
+  //   error: boldRed,
+  // );
+  //
+  // // Icon Theme
+  // static const IconThemeData _lightIconTheme = IconThemeData(
+  //   color: nordicGrey,
+  // );
+  //
+  // // Text Theme
+  // static const TextTheme _lightTextTheme = TextTheme(
+  //   titleLarge: titleLarge,
+  //   bodyLarge: bodyLarge,
+  //
+  //   // titleMedium, titleSmall, bodyMedium, bodySmall, etc
+  // );
 
-  // Text Styles 文本样式
-  static const TextStyle appBarTextStyle = TextStyle(
-    fontSize: 20.0,
-    color: mercuryWhite,
-    fontWeight: FontWeight.bold,
+  // Themes 主题
+  // static final ThemeData lightTheme = ThemeData(
+  //   scaffoldBackgroundColor: mercuryWhite,
+  //   appBarTheme: _lightAppBarTheme,
+  //   colorScheme: _lightColorScheme,
+  //   iconTheme: _lightIconTheme,
+  //   textTheme: _lightTextTheme,
+  // );
+
+  // DG
+  static InputDecoration recipeDropdownMenu = InputDecoration(
+    fillColor: AppTheme.backgroundWhite,
+    filled: true,
+    prefixIcon: const Icon(Icons.soup_kitchen, color: mainGreen),
+    suffixIcon: const Icon(Icons.keyboard_arrow_down, color: mainGreen),
+    enabledBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: backgroundWhite),
+      borderRadius: BorderRadius.circular(15),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: const BorderSide(color: backgroundWhite),
+      borderRadius: BorderRadius.circular(15),
+    ),
   );
 
-  static const TextStyle titleLarge = TextStyle(
-    fontSize: 24.0,
-    color: mercuryWhite,
-    fontWeight: FontWeight.bold,
-  );
+  // JL
+  static const mainGreen = Color(0xff25995C);
+  static const backgroundWhite = Color(0xffFAFAFA);
 
-  static const TextStyle bodyLarge = TextStyle(
-    fontSize: 16.0,
-    color: nordicGrey,
-    fontWeight: FontWeight.bold,
-  );
+  static BoxDecoration widgetDeco({Color color = Colors.white}) {
+    return BoxDecoration(
+      color: color,
+      borderRadius: BorderRadius.circular(15),
+      border: Border.all(color: Colors.grey.withOpacity(0.30)),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.2),
+          spreadRadius: 2,
+          blurRadius: 3,
+          offset: Offset(0, 3), // changes position of shadow
+        ),
+      ],
+    );
+  }
 
-  // AppBar
-  static const AppBarTheme _lightAppBarTheme = AppBarTheme(
-    backgroundColor: nordicGrey,
-    foregroundColor: mercuryWhite,
-    titleTextStyle: appBarTextStyle,
+  static TextStyle buttonText() {
+    return TextStyle(
+        color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600);
+  }
+
+  static AppBarTheme _lightAppBarTheme = AppBarTheme(
+    centerTitle: true,
+    backgroundColor: backgroundWhite,
+    surfaceTintColor: Colors.transparent,
+    titleTextStyle: GoogleFonts.nunito(
+        fontSize: 20, color: Colors.black, fontWeight: FontWeight.w600),
     elevation: 0,
     iconTheme: IconThemeData(color: mercuryWhite),
   );
@@ -64,10 +145,59 @@ class AppTheme {
 
   // Themes 主题
   static final ThemeData lightTheme = ThemeData(
-    scaffoldBackgroundColor: mercuryWhite,
-    appBarTheme: _lightAppBarTheme,
-    colorScheme: _lightColorScheme,
-    iconTheme: _lightIconTheme,
-    textTheme: _lightTextTheme,
-  );
+      colorScheme: ColorScheme.fromSeed(seedColor: mainGreen),
+      primarySwatch: generateMaterialColor(mainGreen),
+      scaffoldBackgroundColor: backgroundWhite,
+      appBarTheme: _lightAppBarTheme,
+      iconTheme: _lightIconTheme,
+      textTheme: GoogleFonts.nunitoTextTheme()
+      //     .copyWith(
+      //   displayLarge: TextStyle(fontWeight: FontWeight.w500),
+      //   displayMedium: TextStyle(fontWeight: FontWeight.w500),
+      //   displaySmall: TextStyle(fontWeight: FontWeight.w500),
+      //   headlineMedium: TextStyle(fontWeight: FontWeight.w500),
+      //   headlineSmall: TextStyle(fontWeight: FontWeight.w500),
+      //   titleLarge: TextStyle(fontWeight: FontWeight.w500),
+      //   titleMedium: TextStyle(fontWeight: FontWeight.w500),
+      //   titleSmall: TextStyle(fontWeight: FontWeight.w500),
+      //   bodyLarge: TextStyle(fontWeight: FontWeight.w500),
+      //   bodyMedium: TextStyle(fontWeight: FontWeight.w500),
+      //   bodySmall: TextStyle(fontWeight: FontWeight.w500),
+      //   labelLarge: TextStyle(fontWeight: FontWeight.w500),
+      //   labelSmall: TextStyle(fontWeight: FontWeight.w500),
+      // ),
+      );
 }
+
+MaterialColor generateMaterialColor(Color color) {
+  return MaterialColor(color.value, {
+    50: tintColor(color, 0.9),
+    100: tintColor(color, 0.8),
+    200: tintColor(color, 0.6),
+    300: tintColor(color, 0.4),
+    400: tintColor(color, 0.2),
+    500: color,
+    600: shadeColor(color, 0.1),
+    700: shadeColor(color, 0.2),
+    800: shadeColor(color, 0.3),
+    900: shadeColor(color, 0.4),
+  });
+}
+
+int tintValue(int value, double factor) =>
+    max(0, min((value + ((255 - value) * factor)).round(), 255));
+
+Color tintColor(Color color, double factor) => Color.fromRGBO(
+    tintValue(color.red, factor),
+    tintValue(color.green, factor),
+    tintValue(color.blue, factor),
+    1);
+
+int shadeValue(int value, double factor) =>
+    max(0, min(value - (value * factor).round(), 255));
+
+Color shadeColor(Color color, double factor) => Color.fromRGBO(
+    shadeValue(color.red, factor),
+    shadeValue(color.green, factor),
+    shadeValue(color.blue, factor),
+    1);
