a58520971b47d2aadc7a435791b52548c171af3e
ohshuzanne <suzannelts02@gmail.com>
2024-07-31 22:54:14 +0800

    fixed fridge page

--- lib/screen/fridge/fridge.dart
+++ lib/screen/fridge/fridge.dart
@@ -1,5 +1,39 @@
 import 'package:flutter/material.dart';
 
+void dialogPopUp(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return Dialog(
+        backgroundColor: Colors.transparent,
+        child: Stack(
+          children: [
+            AlertDialog(
+              title: Text('Confirm'),
+              content: Text('Are you sure you want to proceed?'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(true);
+                  },
+                  child: Text('Yes'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(false);
+                  },
+                  child: Text('No'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
+
 class FridgePage extends StatefulWidget {
   const FridgePage({super.key});
 
@@ -27,19 +61,9 @@ class _FridgePageState extends State<FridgePage> {
           //add icons
           Positioned(
             left: screenWidth * 0.4,
-            top: screenHeight * 0.25,
-            child: Container(
-              height: 60,
-              width: 100,
-              child: Image.asset('lib/images/addicon.png'),
-            ),
-          ),
-
-          Positioned(
-            left: screenWidth * 0.31,
-            top: screenHeight * 0.42,
-            child: Transform.rotate(
-              angle: -0.3,
+            top: screenHeight * 0.22,
+            child: GestureDetector(
+              onTap: () => dialogPopUp(context),
               child: Container(
                 height: 60,
                 width: 100,
@@ -49,14 +73,33 @@ class _FridgePageState extends State<FridgePage> {
           ),
 
           Positioned(
+            left: screenWidth * 0.31,
+            top: screenHeight * 0.42,
+            child: Transform.rotate(
+              angle: -0.3,
+              child: GestureDetector(
+                onTap: () => dialogPopUp(context),
+                child: Container(
+                  height: 60,
+                  width: 100,
+                  child: Image.asset('lib/images/addicon.png'),
+                ),
+              ),
+            ),
+          ),
+
+          Positioned(
             left: screenWidth * 0.48,
             top: screenHeight * 0.53,
             child: Transform.rotate(
               angle: 0.6,
-              child: Container(
-                height: 60,
-                width: 100,
-                child: Image.asset('lib/images/addicon.png'),
+              child: GestureDetector(
+                onTap: () => dialogPopUp(context),
+                child: Container(
+                  height: 60,
+                  width: 100,
+                  child: Image.asset('lib/images/addicon.png'),
+                ),
               ),
             ),
           ),
