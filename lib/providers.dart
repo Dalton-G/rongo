38d3d8236fe2ba0d165030e121a75efa4e453607
Lee Wen Han <leegame12138@gmail.com>
2024-08-04 00:01:18 +0800

    feat: add provider to navigate from inventory -> scanner

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,6 +1,7 @@
 import 'package:provider/provider.dart';
 import 'package:rongo/provider/Item_provider.dart';
 import 'package:rongo/provider/onboarding_provider.dart';
+import 'package:rongo/screen/home/homepage.dart';
 
 class Providers {
   Providers._();
@@ -11,6 +12,9 @@ class Providers {
     ),
     ChangeNotifierProvider<ItemProvider>(
       create: (_) => ItemProvider(),
+    ),
+    ChangeNotifierProvider<IndexProvider>(
+      create: (_) => IndexProvider(),
     ),
   ].toList();
 }
--- lib/screen/inventory/inventory_listview.dart
+++ lib/screen/inventory/inventory_listview.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:rongo/utils/utils.dart';
 import 'package:intl/intl.dart';
 
 import '../../utils/theme/theme.dart';
+import '../home/homepage.dart';
 
 class InventoryListview extends StatefulWidget {
   const InventoryListview({super.key});
@@ -54,71 +56,110 @@ class _InventoryListviewState extends State<InventoryListview> {
 
         //TODO: Handle if desiredCategory? is null
         body: desiredCategory!.isNotEmpty? ListView.builder(
-          itemCount: desiredCategory?.length,
+          itemCount: desiredCategory!.length + 1,
           itemBuilder: (context, index) {
+
+            if (index == (desiredCategory!.length) ) {
+
+              return Padding(
+                padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                child: GestureDetector(
+                  onTap: ((){
+                    Navigator.popUntil(context, ModalRoute.withName('/homepage'));
+                    Provider.of<IndexProvider>(context, listen: false).setSelectedIndex(3);
+                  }),
+                  child: Container(
+                    decoration: AppTheme.widgetDeco(),
+                    padding: EdgeInsets.all(15),
+                    child: const SizedBox(
+                      height: 80,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.add_outlined),
+                          Text("Add new item")
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            }
+
+
             var item = desiredCategory?[index];
             DateFormat format = DateFormat("yyyy-MM-dd");
             DateTime addDate = format.parse(item['addedDate']);
             DateTime expiryDate = item['expiryDate'];
 
-            return Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-              child: Container(
-                decoration: AppTheme.widgetDeco(),
-                padding: EdgeInsets.all(15),
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: 100,
-                      height: 80,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: item['imageDownloadURL'] == null
-                            ? Image.asset("lib/images/avocado.png")
-                            : Image.network(
-                                item['imageDownloadURL'],
-                                fit: BoxFit.cover,
-                              ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 15,
-                    ),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                item['name'] ?? 'Unknown',
-                                style: TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.w600),
-                              ),
-                              SizedBox(
-                                height: 8,
-                              ),
-                              Text(
-                                'Added: ${DateFormat("d MMM y (E)").format(addDate)}\n'
-                                'Expiry: ${DateFormat("d MMM y (E)").format(expiryDate)}',
-                                style: const TextStyle(fontSize: 11, height: 2),
-                              ),
-                            ],
+
+
+              return Padding(
+                padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                child: Container(
+                  decoration: AppTheme.widgetDeco(),
+                  padding: EdgeInsets.all(15),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        height: 80,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: item['imageDownloadURL'] == null
+                              ? Image.asset("lib/images/avocado.png")
+                              : Image.network(
+                            item['imageDownloadURL'],
+                            fit: BoxFit.cover,
                           ),
-                          Text(
-                            "x${item['quantity']}",
-                          )
-                        ],
+                        ),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  item['name'] ?? 'Unknown',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  'Added: ${DateFormat("d MMM y (E)").format(
+                                      addDate)}\n'
+                                      'Expiry: ${DateFormat("d MMM y (E)")
+                                      .format(expiryDate)}',
+                                  style: const TextStyle(
+                                      fontSize: 11, height: 2),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              "x${item['quantity']}",
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            );
+              );
+
+
+
           },
         ):
             Center(child: Text(nullPrompting??""))
