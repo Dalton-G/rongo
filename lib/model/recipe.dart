1784e8ca9f7a254c5cccb2350055a493eab74f8c
Dalton Gan <study.daltongan@gmail.com>
2024-08-09 01:01:41 +0800

    feat: added recipe generation functionality

--- lib/model/recipe.dart
+++ lib/model/recipe.dart
@@ -2,9 +2,6 @@ class Recipe {
   final String name;
   final String description;
   final String imageUrl;
-  final List<String> ingredients;
-  final List<String> steps;
-  final List<String> nutritionalValue;
   final String cookingTime;
   final List<String> tags;
 
@@ -12,9 +9,6 @@ class Recipe {
     required this.name,
     required this.description,
     required this.imageUrl,
-    required this.ingredients,
-    required this.steps,
-    required this.nutritionalValue,
     required this.cookingTime,
     required this.tags,
   });
